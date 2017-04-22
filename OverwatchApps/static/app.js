9174f8945ed834719a482ff69fbc99db4d86da00
dustin-chan-89 <busterdust@gmail.com>
2017-04-22 12:57:09 -0700

    installed pytrends

--- OverwatchApps/static/app.js
+++ OverwatchApps/static/app.js
@@ -2,17 +2,22 @@ $(function () {
   var myChart = Highcharts.chart('container', {
 
       title: {
-          text: 'Solar Employment Growth by Sector, 2010-2016'
+          text: 'Standard Deviation of Search Volume'
       },
 
       subtitle: {
-          text: 'Source: thesolarfoundation.com'
+          text: 'Source: Google Trends'
       },
 
       yAxis: {
           title: {
-              text: 'Number of Employees'
+              text: 'σ'
           }
+      },
+      xAxis: {
+        title: {
+          text: 'date'
+        }
       },
       legend: {
           layout: 'vertical',
@@ -22,7 +27,7 @@ $(function () {
 
       plotOptions: {
           series: {
-              pointStart: 2010
+              pointStart: 0
           }
       },
 
@@ -32,16 +37,18 @@ $(function () {
       }, {
           name: 'Manufacturing',
           data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-      }, {
-          name: 'Sales & Distribution',
-          data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-      }, {
-          name: 'Project Development',
-          data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-      }, {
-          name: 'Other',
-          data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-      }]
+        }
+      ]
+      // }, {
+      //     name: 'Sales & Distribution',
+      //     data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+      // }, {
+      //     name: 'Project Development',
+      //     data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+      // }, {
+      //     name: 'Other',
+      //     data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+      // }]
 
   });
 });
