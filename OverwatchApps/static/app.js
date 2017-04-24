ff8e5af495b191ac329e2b58fad7c37d244bad0c
dustin-chan-89 <busterdust@gmail.com>
2017-04-23 23:43:57 -0700

    slack alert on high stdev

--- OverwatchApps/static/app.js
+++ OverwatchApps/static/app.js
@@ -3,30 +3,53 @@ $(function () {
   var coinDataWeek = JSON.parse(mainCoinDataWeek);
   var coinDataMonth = JSON.parse(mainCoinDataMonth);
 
-  var bitcoinDataDay = convertDataToHighChartFormat(coinDataDay.bitcoin);
-  console.log(bitcoinDataDay);
-  var ethereumDataDay = convertDataToHighChartFormat(coinDataDay.ethereum);
-  var litecoinDataDay = convertDataToHighChartFormat(coinDataDay.litecoin);
+  var bitcoinDataDay = convertDataToHighChartFormat(coinDataDay.bitcoin, 'Bitcoin', true);
+  var ethereumDataDay = convertDataToHighChartFormat(coinDataDay.ethereum, 'Ethereum', true);
+  var litecoinDataDay = convertDataToHighChartFormat(coinDataDay.litecoin, 'Litecoin', true);
 
-  var bitcoinDataWeek = convertDataToHighChartFormat(coinDataWeek.bitcoin);
-  var ethereumDataWeek = convertDataToHighChartFormat(coinDataWeek.ethereum);
-  var litecoinDataWeek = convertDataToHighChartFormat(coinDataWeek.litecoin);
+  var bitcoinDataWeek = convertDataToHighChartFormat(coinDataWeek.bitcoin, 'Bitcoin', false);
+  var ethereumDataWeek = convertDataToHighChartFormat(coinDataWeek.ethereum, 'Ethereum', false);
+  var litecoinDataWeek = convertDataToHighChartFormat(coinDataWeek.litecoin, 'Litecoin', false);
 
-  var bitcoinDataMonth = convertDataToHighChartFormat(coinDataMonth.bitcoin);
-  var ethereumDataMonth = convertDataToHighChartFormat(coinDataMonth.ethereum);
-  var litecoinDataMonth = convertDataToHighChartFormat(coinDataMonth.litecoin);
+  var bitcoinDataMonth = convertDataToHighChartFormat(coinDataMonth.bitcoin, 'Bitcoin', false);
+  var ethereumDataMonth = convertDataToHighChartFormat(coinDataMonth.ethereum, 'Ethereum', false);
+  var litecoinDataMonth = convertDataToHighChartFormat(coinDataMonth.litecoin, 'Litecoin', false);
 
   makeHighChart(bitcoinDataDay, litecoinDataDay, ethereumDataDay, 'day-container', 'Past 24 Hours');
   makeHighChart(bitcoinDataWeek, litecoinDataWeek, ethereumDataWeek, 'week-container', 'Past Week');
   makeHighChart(bitcoinDataMonth, litecoinDataMonth, ethereumDataMonth, 'month-container', 'Past Month');
 
-  function convertDataToHighChartFormat(coinDict) {
+  var slackUrl = "https://hooks.slack.com/services/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";// Webhook URL
+
+  setInterval(function() {
+      window.location.reload();
+    }, 10000); 
+
+  function convertDataToHighChartFormat(coinDict, coinName, isHourly) {
     var keys = Object.keys(coinDict);
     var vals = convertValsToStdDevs(Object.values(coinDict));
     var zipped = keys.map(function(time, i) {
       return [parseInt(time) - 25200000, vals[i]];
     });
+
+    if (isHourly) {
+      var latestStdDevVal = zipped[zipped.length - 1][1];
+      if (latestStdDevVal >= 2.5) {
+        var text = coinName + ' is experiencing an unusually high vol. of searches (' + latestStdDevVal + ')'; // Text to post
+        $.ajax({
+            data: 'payload=' + JSON.stringify({
+                "text": text
+            }),
+            dataType: 'json',
+            processData: false,
+            type: 'POST',
+            url: slackUrl
+        });
+      }
+    }
+
     return zipped;
+
   }
 
   function arraySum(arr) {
